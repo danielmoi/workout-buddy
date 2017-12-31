42bd5e92a31c9db7cd95f55afa3cf139f73a2d07
danielmoi <daniel@ampersandmoi.com>
2018-01-01 10:02:53 +1100

    Add exercise details VC

---
+++ WorkoutBuddy/ExerciseViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ExerciseViewController.swift
+//  WorkoutBuddy
+//
+//  Created by Daniel Moi on 1/1/18.
+//  Copyright © 2018 Daniel Moi. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
